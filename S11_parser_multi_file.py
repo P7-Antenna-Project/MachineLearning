72eb2e277c116871eee8217fef03a2d0a150e947
NicolaiLyholm <Nlyholm@hotmail.com>
2023-10-24 12:16:20 +0200

    ...

---
+++ S11_parser_multi_file.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import pickle
+
+x = np.loadtxt('MachineLearning\data\data112.csv', delimiter=',') 
+
+# Making list to store s11 values for each run
+s11_list = []
+
+# Loop through the files
+for i in tqdm(range(0, len(x))):  
+    filename = f"MachineLearning/data/s11/s11file_{i}.txt"    
+    
+    with open(filename, 'r') as file:
+        file.readline()  # Skip the first line
+        file.readline()  # Skip the second line
+        s11_values =[] # Create an empty array to store the s11 values
+        frequency = [] # Create list to store frequency values (The don't vary between runs)
+        
+        # Loop through the lines in the file
+        for line in file:
+            if line != '\n':  # Skip the empty line
+                parts = line.split()
+                s11_values.append(float(parts[1]))
+                if i == 0: # We only need to store the frequency values once
+                 frequency.append(float(parts[0]))
+                           
+
+    s11_list.append(s11_values)
+    
+# Add the parameter combinations and s11 values to the dictionary
+file_data = {'Parameter combination': x, 'S1,1': np.asarray(s11_list)}
+
+#Pickle the dictionary into a file
+with open('MachineLearning/data/Data_dict.pkl', 'wb') as pkl_path: #Dump the pickle file at the given file path
+    pickle.dump(file_data, pkl_path)
+    
+
+# print(f"The parameter combination is: {file_data['Parameter combination'][2]}, the s11 values are: {file_data['S1,1'][2]}")
