1d6dfac2af2cb9ca80c56fa324e351e136284652
NicolaiLyholm <awgaouwehgiub@gmail.com>
2023-11-15 16:33:30 +0100

    Fixed get_radiation_data.py

--- get-radiation-data.py
+++ get-radiation-data.py
@@ -10,37 +10,38 @@ dim_x = len(paraname)
 para_min = np.array([157/10,4,0.5])
 para_max = np.array([157,15,3])
 para_step = np.array([7.43684210526316, 1, 0.25])
-ns = np.array([(para_max[0]-para_min[0])/para_step[0]+1,(para_max[1]-para_min[1])/para_step[1]+1,(para_max[2]-para_min[2])/para_step[2]+1]).astype(np.int) #Number of parameter samples
+ns = np.array([(para_max[0]-para_min[0])/para_step[0]+1,(para_max[1]-para_min[1])/para_step[1]+1,(para_max[2]-para_min[2])/para_step[2]+1]).astype(int) #Number of parameter samples
 num = np.prod(ns)
 
-laptop_path = r"C:\Program Files (x86)\CST Studio Suite 2021\AMD64\python_cst_libraries"
+laptop_path = r"C:\Program Files (x86)\CST Studio Suite 2023\AMD64\python_cst_libraries"
 sys.path.append(laptop_path)
 import cst.interface
 import shutil
 
 # path til cst-filen:
-cst_path =  r"C:\Users\madsl\Dropbox\AAU\EIT 7. sem\P7\Python6_stuff\MachineLearning\CST files\Wire_antenna_simple_2.cst"
+#cst_path =  r"C:\Users\madsl\Dropbox\AAU\EIT 7. sem\P7\Python6_stuff\MachineLearning\CST files\Wire_antpyenna_simple_2.cst" (NOT USED)
 def represent(para):
     return para/(ns-1)*(para_max-para_min)+para_min
 
 para = np.zeros((num, dim_x))
 para[:, -1] = np.arange(num)
 for j in range(dim_x):
-    para[:, j] = (np.mod(para[:, -1], np.prod(ns[j:]))/np.prod(ns[j+1:])).astype(np.int)
+    para[:, j] = (np.mod(para[:, -1], np.prod(ns[j:]))/np.prod(ns[j+1:])).astype(int)
 para = represent(para)
 pd.DataFrame(para).to_csv(f'data\\distanceTest{num}.csv', header=None, index=None)
 
 
-new_cstfile = r"C:\Users\madsl\Dropbox\AAU\EIT 7. sem\P7\Python6_stuff\MachineLearning\CST files\Wire_antenna_simple_2.cst"
+new_cstfile = r"C:\Users\nlyho\OneDrive - Aalborg Universitet\7. semester\Git\MachineLearning\CST files\Wire_antenna_simple_2.cst"
 DE = cst.interface.DesignEnvironment()
 microwavestructure = DE.open_project(new_cstfile)
 modeler = microwavestructure.modeler
 schematic = microwavestructure.schematic
-start = 1899
+start = 0
+num = 5
 # generate vba code that changes the parameters in cst
 def setpara(paraname, paravalue, num=dim_x):
     code = ''
-    for i in range(start,num):
+    for i in range(0,num):
         code += 'StoreParameter("{}", {})'.format(paraname[i], paravalue[i])
         if i < num - 1:
             code += '\n'
@@ -165,7 +166,7 @@ angles = [0,45,90]
 for i in range(start, num):
     print(f'Run {i+1}/{num}:')
     toc = time.time()
-    s11file =f'C:/Users/madsl/Dropbox/AAU/EIT 7. sem/P7/Python6_stuff/data/wireAntennaSimple2Results/s11/s11_{i}.txt'
+    s11file =f'C:/Users/nlyho/OneDrive - Aalborg Universitet/7. semester/Git/MachineLearning/data/wireAntennaSimple2Results_nicolai/test_s11/s11_{i}.txt'
     
     # for j in angles:
     #     theta = f'data//radiation//theta//theta{j}_{i}.txt'
@@ -173,17 +174,17 @@ for i in range(start, num):
 
 # "C:\Users\madsl\Dropbox\AAU\EIT 7. sem\P7\Python6_stuff\data\wireAntennaSimple2Results\theta"
 
-    phi0 = f'data/wireAntennaSimple2Results/theta/phi0_{i}.txt'
-    phi45= f'data/wireAntennaSimple2Results/theta/phi45_{i}.txt'
-    phi90 = f'data/wireAntennaSimple2Results/theta/phi90_{i}.txt'
-    theta0 = f'data/wireAntennaSimple2Results/theta/theta0_{i}.txt'
-    theta45 = f'data/wireAntennaSimple2Results/theta/theta45_{i}.txt'
-    theta90 = f'data/wireAntennaSimple2Results/theta/theta90_{i}.txt'
+    phi0 = f'C:/Users/nlyho/OneDrive - Aalborg Universitet/7. semester/Git/MachineLearning/data/wireAntennaSimple2Results_nicolai/test_phi/phi0_{i}.txt'
+    phi45= f'C:/Users/nlyho/OneDrive - Aalborg Universitet/7. semester/Git/MachineLearning/data/wireAntennaSimple2Results_nicolai/test_phi/phi45_{i}.txt'
+    phi90 = f'C:/Users/nlyho/OneDrive - Aalborg Universitet/7. semester/Git/MachineLearning/data/wireAntennaSimple2Results_nicolai/test_phi/phi90_{i}.txt'
+    theta0 = f'C:/Users/nlyho/OneDrive - Aalborg Universitet/7. semester/Git/MachineLearning/data/wireAntennaSimple2Results_nicolai/test_theta/theta0_{i}.txt'
+    theta45 = f'C:/Users/nlyho/OneDrive - Aalborg Universitet/7. semester/Git/MachineLearning/data/wireAntennaSimple2Results_nicolai/test_theta/theta45_{i}.txt'
+    theta90 = f'C:/Users/nlyho/OneDrive - Aalborg Universitet/7. semester/Git/MachineLearning/data/wireAntennaSimple2Results_nicolai/test_theta/theta90_{i}.txt'
     
     #r'C:/Users/nlyho/OneDrive - Aalborg Universitet/7. semester/CSTEnv/data/s11/s11file.txt'
     # f'C:/Users/nlyho/OneDrive - Aalborg Universitet/7. semester/CSTEnv/data/s11/s11file_{i}.txt'
     
     s21file =f'data//s21//s21_{i}.txt'
     tryrun(paraname, para[i], dim_x, s11file, s21file,phi0,phi45,phi90, theta0, theta45, theta90)
-    
+    print(para[i])
     print(f'{time.time()-toc} used for this run, {time.time()-tic} used, {(time.time()-tic)/(i+1-start)*(num-i-1)} more needed')
