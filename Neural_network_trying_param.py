d80a7cc88a34f794c8a7f289da5de6c33366135e
AppelEater <david@vajo.dk>
2023-12-04 08:56:56 +0100

    Co-authored-by: Nicolai Lyholm <NicolaiLyholm@users.noreply.github.com>

---
+++ Neural_network_trying_param.py
@@ -0,0 +1,183 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers
+import pickle
+from sklearn.model_selection import train_test_split
+import os
+import shutil
+import random
+import time
+
+# Flags
+PLOT_TRAIN = True
+PLOT_TEST = True
+PLOT_TEST_LOSS = True
+
+def reset_dirs():
+    for layer in range(10):
+        shutil.rmtree(f'data/DNN_results/train_loss/train_loss_layer{layer+1}', ignore_errors=True)
+        shutil.rmtree(f'data/DNN_results/test_pred/test_pred_layer{layer+1}', ignore_errors=True)
+        
+        os.mkdir(f'data/DNN_results/train_loss/train_loss_layer{layer+1}')
+        os.mkdir(f'data/DNN_results/test_pred/test_pred_layer{layer+1}')
+    return
+
+def load_data(path: str):
+    with open(path,'rb') as file:
+        data_dict = pickle.load(file)
+
+
+    print(data_dict.keys())
+    par_comb = np.asarray(data_dict['Parameter combination'])
+    frequency = np.asarray(data_dict['Frequency'])
+    S11_par = np.asarray(data_dict['S1,1'])
+    Parametric_S11 = np.asarray(data_dict['Parametric S1,1'])
+
+    return par_comb, S11_par, Parametric_S11, frequency
+
+def normalize_data(data: dict, inverse: bool):
+    if inverse:
+        data_norm = data*np.std(data) + np.mean(data)
+    else:   
+        mean = np.mean(data)
+        std = np.std(data)
+        data_norm = (data-mean)/std
+    return data_norm
+
+
+# Run main code
+if __name__ == "__main__":
+        # Ready the directories
+    reset_dirs()
+
+    # Load the data
+    par_comb, S11_par, parametric, frequency = load_data("data/simple_wire2_final_with_parametric.pkl")
+
+    x = par_comb
+
+    #print(parametric.shape, parametric)
+
+    print(parametric[0])
+
+    y = np.asarray([np.concatenate([x[0], x[1]])for x in parametric])
+    print(y.shape, y[0])
+
+    # Define training and test data
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, shuffle=True)
+
+    #normalize the data (maybe a keras normalization layer is better)
+    xmean1 = np.mean(x_train)
+    xstd1 = np.std(x_train)
+    xmean2 = np.mean(x_test)
+    xstd2 = np.std(x_test)
+
+    x_train_norm = x_train
+    x_test_norm = x_test
+
+    y_train_norm = y_train
+    y_test_norm = y_test
+
+    print(y_train_norm.shape[1])
+
+    # List containing the layers of the model, will have layers appended with each iteration
+    base_layers = [
+        layers.InputLayer(input_shape=(3)),
+        layers.Dense(y_train_norm.shape[1], activation = 'linear', name = 'Output_layer')
+    ]
+    # Create list to store run times
+    run_time = np.zeros(10)
+
+    for layer in range(4):
+        start_time = time.time()
+        # Add a layer to the model
+        base_layers.insert(-1, layers.Dense(128, activation='sigmoid', name = f'layer{layer+1}'))
+        
+        # Create the model
+        model = keras.Sequential(base_layers,name=f'Sequential_{layer+1}')
+        model.summary()
+
+        # Compile the model with the solver adam and the loss function MSE
+        model.compile(
+            optimizer=keras.optimizers.Adam(learning_rate=0.001),
+            loss=keras.losses.MeanAbsoluteError(),
+            metrics=[keras.metrics.MeanSquaredError()]
+        )
+        
+        # Create lists to store the results
+        loss_train = []
+        mean_error_train = []
+        mean_error_pred = np.zeros(50)
+        # Train the model
+        for j in range(10):
+            model.fit(
+                x=x_train_norm,
+                y=y_train_norm,
+                batch_size=100,
+                epochs=100,
+                shuffle=True,
+                callbacks=[keras.callbacks.History()],
+                verbose=1)
+
+            # Define the loss and accuracy for the training and test data
+            loss_train.extend(model.history.history['loss'])
+            mean_error_train.extend(model.history.history['mean_squared_error'])
+            
+            if PLOT_TRAIN:
+                plt.figure()
+                plt.subplots_adjust(wspace=0.5)
+                plt.subplot(121)
+                plt.plot(np.array(loss_train).T)
+                plt.ylabel('Absolute error')
+                plt.xlabel('epoch')
+                plt.legend(['Absolute error'])
+                plt.ylim([0, 1])
+                plt.subplot(122)
+                plt.plot(np.array(mean_error_train).T)
+                plt.ylabel('Mean-squared error')
+                plt.xlabel('epoch')
+                plt.legend(['Mean-squared error'])
+                plt.ylim([0, 1])
+                plt.savefig(f'data/DNN_results/train_loss/train_loss_layer{layer+1}/loss_{(j+1)*100}.png')
+                plt.close()
+            # Run the model on the test data and get the loss and mean-squared error
+            y_pred_norm = model.predict(x_test_norm)
+            _ , mean_error_pred[j] = model.evaluate(x_test_norm, y_test_norm)
+            
+            
+            # Reverse the normalization of the labels
+            y_pred = normalize_data(y_pred_norm, inverse=True)
+
+            # Plot the testing results
+            if PLOT_TEST:
+                # Generate 10 unique random indices
+                random_indices = random.sample(range(len(y_pred)), 10)
+
+                plt.figure(figsize=(50, 50))
+                for idx, i in enumerate(random_indices):
+                    plt.subplot(5, 2, idx+1)
+                    plt.plot(y_pred[i], label='pred')
+                    plt.plot(y_test[i], label='test')
+                    plt.legend()
+                    plt.grid(True)
+                    plt.ylim([-40,2])
+                plt.savefig(f'data/DNN_results/test_pred/test_pred_layer{layer+1}/test_pred_{(j+1)*100}.png')
+                plt.close()
+
+        # Plot the testing loss
+        if PLOT_TEST_LOSS:
+            plt.plot(np.arange(1,51,1)*100, mean_error_pred, label = f'layer{layer+1}')
+            plt.ylabel('Mean-squared error')
+            plt.xlabel('epoch')
+            plt.legend()
+            plt.grid(True)
+
+        # Save the run time for the current model
+        run_time[layer] = time.time() - start_time
+        # Save the model
+        model.save(f'data/DNN_results/Models/forward_model_{layer+1}_layer.keras', overwrite=True)
+    plt.savefig(f'data/DNN_results/test_loss/test_loss_{layer+1}_layer.png')
+    plt.close()
+    np.savetxt('data/DNN_results/run_time.txt', run_time)
+    
--- testing_of_reverse_model.py
+++ testing_of_reverse_model.py
@@ -4,12 +4,81 @@ from sklearn.model_selection import train_test_split
 import pickle
 import numpy as np
 import matplotlib.pyplot as plt
+import random
+from scipy.optimize import curve_fit
 
-model_path = 'Models/Reverse_Neural_network.keras'
+def import_s11_data(filename):
+    filename = filename  
+            #print(filename) #Debugging
+    with open(filename, 'r') as file:
+        file.readline()  # Skip the first line
+        file.readline()  # Skip the second line
+        file.readline()  # Skip the third line
+        s11_values = [] # Create an empty array to store the s11 values
+
+        # Loop through the lines in the file
+        for line in file:
+            if line != '\n':  # Skip the empty lines
+                parts = line.split()
+                s11_values.append(float(parts[1]))
+
+    return s11_values
+
+
+def make_s11_curve(BW, center_freq, frequency):
+    # Define frequency range
+    frequency = frequency
+    
+    def rational_func(x, *args):
+
+        def rational(data, q, p):
+            return np.polyval(q, data) / np.polyval(p + [1.0], data)
+
+        # Split the args into poles and zeros
+        zeros = args[:len(args)//2]
+        poles = args[len(args)//2:]
+        #print(poles, zeros)  # Debugging line
+        return rational(x, [*zeros], [*poles])
+
+    # Define points for the S11 curve based on BW and center frequency
+    points = []
+
+    depth = -16
+    a = (depth-(-10))/(BW/2)
+    print(a)
+    b1 = depth - a*center_freq
+    b2 = depth + a*center_freq
+    base_line = -3
+    for i in frequency:
+
+        def in_band(x):
+            #print('in band called')
+            return a*x + b1 if x < center_freq else -a*x + b2
+
+        #print((-0.2-b2)/(-a))
+        #print((-0.2-b1)/(a))
+        points.append(in_band(i) if i < (base_line-b2)/(-a) and i > (base_line-b1)/(a) else base_line)
+        
+    
+
+    #plt.plot(frequency, points, label='S11 box curve')
+    zeros = np.linspace(int(10e3),int(10e6), 4)
+    poles = np.linspace(int(10e3),int(10e6), 4)
+
+    fitted_curve, cov_matrix, info_d, _, __ = curve_fit(rational_func, frequency, points, p0=[*zeros, *poles], full_output=True)
+
+    S11_curve = rational_func(frequency, *fitted_curve)
+    
+    plt.plot(frequency, S11_curve, label='S11 fitted curve')
+    return S11_curve
+
+    
+
+model_path = 'Models/Reverse_Neural_network_2.keras'
 model = load_model(model_path)
 
 #Load the data
-with open("data/Simple_wire.pkl", 'rb') as file:
+with open("data/simple_wire2_final_with_parametric.pkl", 'rb') as file:
     data_dict = pickle.load(file)
 
 # Define the input and output data
@@ -17,8 +86,10 @@ y = np.asarray(data_dict['Parameter combination'])
 frequency = np.asarray(data_dict['Frequency'])
 x = np.asarray(data_dict['S1,1'])
 
+S_11_homemade = make_s11_curve(100, 1900, frequency)
+
 # Define training and test data
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, shuffle=False)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, shuffle=True, random_state=42)
 
 xmean1 = np.mean(x_train)
 xstd1 = np.std(x_train)
@@ -31,16 +102,41 @@ ymean1 = np.mean(y_train)
 ystd1 = np.std(y_train)
 
 
+#S_11_for_test = np.asarray(import_s11_data(r'data\results_for_reverse_testingtxt.txt'))
+
+#S_11_for_test[800:] = -3.3
+#plt.plot(frequency, S_11_for_test, label='S11 test curve')
+
+#S_11_for_test_norm = (S_11_for_test-xmean1)/xstd1
+
+#print(f"Parameters of prediction {model.predict(np.asarray([S_11_for_test_norm]))*ystd1 + ymean1}")
+
 x_train_norm = (x_train-xmean1)/xstd1
 x_test_norm = (x_test-xmean1)/xstd1
 
-new_x = []
-for i in frequency:
-    new_x.append(-10 if i < 1950 and i > 1850 else 0)
+S_11_homemade_norm = (S_11_homemade-xmean1)/xstd1
 
-new_x_norm = (new_x-xmean1)/xstd1
+print(model.predict(np.asarray([S_11_homemade_norm]))*ystd1 + ymean1)
 
-print(model.predict(np.asarray([new_x_norm]))*ymean1 + ystd1)
+# Find test curves where the S11 goes below -10 dB and the frequency is below 2 GHz
+test_indices = []
+for idx, i in enumerate(x):
+    if np.min(i) < -10 and frequency[np.argmin(i)] < 2000:
+        test_indices.append(i)
+print(f"Number of test curves that satisfy the condition: {len(test_indices)}")
+random_indices = random.sample(test_indices, 3)
 
-plt.plot(frequency, new_x, label = 'Input')
+new_x_norm = (np.asarray(random_indices)-xmean1)/xstd1
+
+print([model.predict(np.asarray([i]))*ystd1 + ymean1 for i in new_x_norm])
+
+plt.legend()
+plt.grid()
+plt.xlabel('Frequency [MHz]')
+plt.ylabel('S11 [dB]')
 plt.show()
+
+# for idx, i in enumerate(random_indices):
+#     plt.plot(frequency, i, label=f'S11 {idx}')
+# plt.legend()
+# plt.show()
