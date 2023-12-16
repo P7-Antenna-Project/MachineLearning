Fixed normalization for all models, so we are no longer cheating :), made figures for report

MadsNeural_network_500epoch.py
@@ -13,7 +13,13 @@ import time
 import json
 import keras.backend as K
 from keras import regularizers
+import matplotlib.font_manager
 # keep
+
+#Set latex font
+# plt.rcParams.update({'font.family': 'Serif','font.serif': 'CMU Serif', 'font.size': 14})
+
+
 
 path = "C:/Users/nlyho/OneDrive - Aalborg Universitet/7. semester/Git/MachineLearning"
 print(path)
@@ -56,6 +62,7 @@ def load_data(path: str):
     #efficiency = np.asarray(list(data_dict['efficiency'].values()))
     return par_comb, S11_vals, frequency, degrees, combined_gain, std_dev, efficiency
 
+
 def normalize_data(data_input, mean, std_dev, inverse: bool):
     if inverse:
         data = data_input*std_dev + mean
@@ -80,24 +87,30 @@ if __name__ == "__main__":
     path = 'C:/Users/nlyho/OneDrive - Aalborg Universitet/7. semester/Git/MachineLearning/'
     par_comb, S11_vals, frequency, degrees, combined_gain, std_dev, efficiency = load_data(f"C:/Users/nlyho/OneDrive - Aalborg Universitet/7. semester/Git/MachineLearning/data/Wire_Results/simple_wire2_final_with_parametric.pkl")
 
-    # Normalize data
-    par_comb_norm = normalize_data(par_comb,np.mean(par_comb),np.std(par_comb), False)
-    S11_vals_norm = normalize_data(S11_vals,np.mean(S11_vals),np.std(S11_vals), False)
-    # S11_parameterized_norm = normalize_data(S11_parameterized, np.mean(S11_parameterized),np.std(S11_parameterized), False)
-    frequency_norm = normalize_data(frequency, np.mean(frequency),np.std(frequency), False)
-    degrees_norm = normalize_data(degrees, np.mean(degrees),np.std(degrees), False)
-    combined_gain_norm = normalize_data(combined_gain, np.mean(combined_gain),np.std(combined_gain), False)
-    std_dev_norm = normalize_data(std_dev, np.mean(std_dev),np.std(std_dev), False)
-    efficiency_norm = normalize_data(efficiency, np.mean(efficiency),np.std(efficiency), False)
+    # # Normalize data
+    # par_comb_norm = normalize_data(par_comb,np.mean(par_comb),np.std(par_comb), False)
+    # S11_vals_norm = normalize_data(S11_vals,np.mean(S11_vals),np.std(S11_vals), False)
+    # # S11_parameterized_norm = normalize_data(S11_parameterized, np.mean(S11_parameterized),np.std(S11_parameterized), False)
+    # frequency_norm = normalize_data(frequency, np.mean(frequency),np.std(frequency), False)
+    # degrees_norm = normalize_data(degrees, np.mean(degrees),np.std(degrees), False)
+    # combined_gain_norm = normalize_data(combined_gain, np.mean(combined_gain),np.std(combined_gain), False)
+    # std_dev_norm = normalize_data(std_dev, np.mean(std_dev),np.std(std_dev), False)
+    # efficiency_norm = normalize_data(efficiency, np.mean(efficiency),np.std(efficiency), False)
 
 
-    input_vector = par_comb_norm
+    input_vector = par_comb
 
-    output_vector = np.asarray([np.concatenate((S11_vals_norm[i], [std_dev_norm[i]], [efficiency_norm[i]]))for i in range(S11_vals.shape[0])])
+    output_vector = np.asarray([np.concatenate((S11_vals[i], [std_dev[i]], [efficiency[i]]))for i in range(S11_vals.shape[0])])
 
 
     # Split data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(input_vector, output_vector, test_size=0.3, shuffle=True, random_state=42)
+    x_train, x_test, y_train, y_test = train_test_split(input_vector, output_vector, test_size=0.3, shuffle=True, random_state=42)
+
+    x_train_norm = normalize_data(x_train, np.mean(x_train),np.std(x_train), False)
+    x_test_norm = normalize_data(x_test, np.mean(x_test),np.std(x_test), False)
+    y_train_norm = normalize_data(y_train, np.mean(y_train),np.std(y_train), False)
+    y_test_norm = normalize_data(y_test, np.mean(y_test),np.std(y_test), False)
+
 
 
     std_dev = y_test[:,-2]
@@ -126,7 +139,7 @@ if __name__ == "__main__":
         
     # Make a 4 layer neural network using relu activation functions
     model = keras.Sequential()
-    model.add(layers.Dense(256, activation='relu', input_shape=(X_train.shape[1],)))
+    model.add(layers.Dense(256, activation='relu', input_shape=(x_train.shape[1],)))
     model.add(layers.Dense(256, activation='relu',kernel_regularizer=regularizers.l2(0.001)))
     model.add(layers.Dense(256, activation='relu',kernel_regularizer=regularizers.l2(0.001)) )
     model.add(layers.Dense(256, activation='relu', kernel_regularizer=regularizers.l2(0.001)))
@@ -150,8 +163,8 @@ if __name__ == "__main__":
 
 
     # Train the model
-    history = model.fit(X_train,
-                        y_train,
+    history = model.fit(x_train_norm,
+                        y_train_norm,
                         epochs=500,
                         batch_size=100,
                         shuffle=True,
@@ -165,17 +178,20 @@ if __name__ == "__main__":
         plt.figure()
         plt.subplots_adjust(wspace=0.5)
         plt.subplot(121)
+        plt.grid(True)
         plt.plot(np.array(loss_train).T)
-        plt.ylabel('Absolute error')
+        plt.ylabel('Weighted error')
         plt.xlabel('epoch')
-        plt.legend(['Absolute error'])
+        plt.legend(['Weighted error'])
         plt.ylim([0, 1])
         plt.subplot(122)
+        plt.grid(True)
         plt.plot(np.array(mean_error_train).T)
         plt.ylabel('Mean-squared error')
         plt.xlabel('epoch')
         plt.legend(['Mean-squared error'])
         plt.ylim([0, 1])
+        
         
         # For saving the training loss figure
         train_loss_path = os.path.join(path, 'data', 'Wire_Results',f'train_loss_with_regularizer.png').replace("\\", "/")
@@ -184,17 +200,24 @@ if __name__ == "__main__":
         plt.close()
         
         # Run the model on the test data and get the loss and mean-squared error
-        y_pred_norm = model.predict(X_test)
+        y_pred_norm = model.predict(x_test)
         # _ , mean_error_pred = model.evaluate(X_test, y_test)
 
          # Reverse the normalization of the data individually
-        y_pred_s11 = normalize_data(y_pred_norm[:,:1001], np.mean(S11_vals),np.std(S11_vals), inverse=True)
-        std_dev_pred = normalize_data(y_pred_norm[:,-2], np.mean(std_dev), np.std(std_dev), inverse=True)
-        efficiency_pred = normalize_data(y_pred_norm[:,-1], np.mean(efficiency), np.std(std_dev), inverse=True)
+        y_pred = normalize_data(y_pred_norm, np.mean(y_train),np.std(y_train), inverse=True) 
+        
+        # #Split the data into separate performancem metrics
+        # y_pred_s11 = y_pred[:][:1001]
+        # std_dev_pred = y_pred[:,-2]
+        # efficiency_pred = y_pred[:,-1]
+        
+        # y_pred_s11 = normalize_data(y_pred_norm[:,:1001], np.mean(S11_vals),np.std(S11_vals), inverse=True)
+        # std_dev_pred = normalize_data(y_pred_norm[:,-2], np.mean(std_dev), np.std(std_dev), inverse=True)
+        # efficiency_pred = normalize_data(y_pred_norm[:,-1], np.mean(efficiency), np.std(std_dev), inverse=True)
 
-        #Compute MSE of predicted S11 curve and test s11 curve
-        MSE = mean_squared_error(y_pred_s11, y_test[:,:1001])
-        print(f'Test MSE: {MSE}')
+        # #Compute MSE of predicted S11 curve and test s11 curve
+        # MSE = mean_squared_error(y_pred_s11, y_test[:,:1001])
+        # print(f'Test MSE: {MSE}')
         # Find test curves where the S11 goes below -10 dB and the frequency is below 2 GHz
         test_indices = []
         for idx, i in enumerate(y_test[:,:1001]):
@@ -203,13 +226,13 @@ if __name__ == "__main__":
         print(f"Number of test curves that satisfy the condition: {len(test_indices)} within the test set")
 
         # Select 10 random curves from the good test curves
-        random_indices = random.sample(test_indices, 10)
+        random_indices = random.sample(test_indices, 4)
         # random_indices = [321, 673, 43, 209, 495, 629, 868, 151, 755, 578]
-        error_std_dev = np.abs(std_dev - std_dev_pred)
-        MSE_std_dev = mean_squared_error(std_dev, std_dev_pred)
+        error_std_dev = np.abs(std_dev - y_pred[:,-2])
+        MSE_std_dev = mean_squared_error(std_dev, y_pred[:,-2])
     
-        error_efficiency = np.abs(efficiency - efficiency_pred)
-        MSE_efficiency = mean_squared_error(efficiency, efficiency_pred)
+        error_efficiency = np.abs(efficiency - y_pred[:,-1])
+        MSE_efficiency = mean_squared_error(efficiency, y_pred[:,-1])
         
 
         error_dictionary = {'error_std_dev': error_std_dev.tolist(), 'MSE_std_dev': MSE_std_dev.tolist(), 'error_efficiency': error_efficiency.tolist(), 'MSE_efficiency': MSE_efficiency.tolist()}
@@ -221,11 +244,11 @@ if __name__ == "__main__":
 
     # Plot the test data and the predicted data
     if PLOT_TEST: 
-        plt.figure(figsize=(50, 50))
+        plt.figure(figsize=(20, 20))
         for idx, i in enumerate(random_indices):
-            plt.subplot(5, 2, idx+1)
-            plt.plot(frequency, y_pred_s11[i], label='pred')
-            plt.plot(frequency, normalize_data(y_test[i][:1001],np.mean(S11_vals),np.std(S11_vals),True), label='test')
+            plt.subplot(2, 2, idx+1)
+            plt.plot(frequency, y_pred[i][:1001],linewidth = 4, label='pred')
+            plt.plot(frequency, y_test[i][:1001],linewidth = 4, label='test')
             plt.legend()
             plt.grid(True)
             plt.ylim([-40,2])
@@ -241,7 +264,7 @@ if __name__ == "__main__":
             
         run_time = time.perf_counter() - start_time
         run_times.append(run_time)
-        np.savetxt('run_times.txt', run_times)   
+        # np.savetxt('run_times.txt', run_times)   
         
         if PLOT_TEST_LOSS:
             print(f'Test loss: {mean_error_pred}')

Reverse_model2.py
@@ -39,24 +39,24 @@ print(input_vector.shape)
 print(output_vector.shape)
 
 # Split data into training and testing
-X_train, X_test, y_train, y_test = train_test_split(input_vector, output_vector, test_size=0.2, random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(input_vector, output_vector, test_size=0.2, random_state=42)
 
 # Normalise data
-x_train_mean = np.mean(X_train, axis=0)
-x_train_std = np.std(X_train, axis=0)
+x_train_mean = np.mean(x_train, axis=0)
+x_train_std = np.std(x_train, axis=0)
 
 y_train_mean = np.mean(y_train, axis=0)
 y_train_std = np.std(y_train, axis=0)
 
-x_train_norm = (X_train - x_train_mean)/x_train_std
+x_train_norm = (x_train - x_train_mean)/x_train_std
 y_train_norm = (y_train - y_train_mean)/y_train_std
 
-x_test_norm = (X_test - x_train_mean)/x_train_std
+x_test_norm = (x_test - x_train_mean)/x_train_std
 y_test_norm = (y_test - y_train_mean)/y_train_std
 
 # Create model
 model = keras.Sequential([
-    layers.InputLayer(input_shape=(X_train.shape[1])),
+    layers.InputLayer(input_shape=(x_train.shape[1])),
     layers.Dense(256, activation='relu', name = 'layer1'),
     layers.Dense(256, activation='relu', name = 'layer2'),
     layers.Dense(256, activation='relu', name = 'layer3'),
@@ -72,7 +72,7 @@ model.compile(optimizer='adam',
               )
 
 # Train model
-history = model.fit(X_train, y_train, epochs=500, batch_size=100, validation_split = 0.2)
+history = model.fit(x_train, y_train, epochs=500, batch_size=100, validation_split = 0.2)
 
 # Plot training history
 plt.figure()